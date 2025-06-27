35bfd5990a87244afa94ed793f945934ffba1c2c
Gustavo <gustavofpaiva2016@gmail.com>
2025-06-26 21:19:39 -0300

    dia 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,57 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import logo from "@/assets/logo.png"
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+      <main>
+        <section className={styles.sectionWpp}/>
+          
+        
+        <section className={styles.section}>
+          <Image
+            src={logo}  // Caminho relativo à pasta public
+            alt="Logo Paiva Guincho"
+            width={200}                // Largura da imagem
+            height={200}               // Altura da imagem
+          />
+          <div className={styles.containerWpp}>
+            <h2 className={styles.title}>
+              Emergência? Precisa de um guincho agora?
+            </h2>
+            <h4 className={styles.subtitle}>
+              Guincho rápido e confiável: Seu aliado nas estradas
+            </h4>
+            <button className={styles.buttonWpp}>
+              <h4 className={styles.desc}>
+                Whatsapp - CHAME AGORA
+              </h4>
+            </button>
+          </div>
+          <h4 className={styles.desc}>
+            GUINCHO EM SÃO PAULO - SOCORRO RÁPIDO - ATENDO TODAS AS REGIÕES
+          </h4>
+        </section>
+        <section className={styles.section}>
+          <h1>
+            text
+          </h1>
+        </section>
+        <section className={styles.section}>
+          <h1>
+            text
+          </h1>
+        </section>
+        <section className={styles.section}>
+          <h1>
+            text
+          </h1>
+        </section>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer>
+
       </footer>
     </div>
   );
