2dba2c6369f77f0bd3e6fc083d3e65d5842ca2ac
Gustavo Fidencio <gustavofidencio@MacBook-Pro-de-Gustavo.local>
2025-07-17 15:29:54 -0300

    deploy

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/PaivaGuincho',
+  assetPrefix: '/PaivaGuincho',
+};
+
+module.exports = nextConfig;
